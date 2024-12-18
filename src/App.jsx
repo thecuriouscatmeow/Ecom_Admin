d1500b0bd396b25e0364ce75d0634254d4c9f2a2
Saahil Basak <thecuriouscat@Saahils-MacBook-Air.local>
2024-12-18 11:20:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>MedjeeX Ecommerce Admin</h1>
+      {/* NAVBAR */}
+      {/* SEARCHBAR */}
+      {/* PRODUCTS LIST */}
+    </>
+  )
+}
+
+export default App
